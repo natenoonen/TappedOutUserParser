21477b27c1f2fe9df3bd06785d8a7797f980d549
Nate Noonen <nnoonen@kcura.com>
2017-05-08 23:11:09 -0500

    Fixing more defects and incorrect commit

--- TODO.md
+++ TODO.md
@@ -1,3 +1,1 @@
-1. Fix link at the end of card names (&amp;ref=37240967036).  Inconsistent ref number
-2. Fix HTML escaped single quote
-3. Ordering, speed increases, writeup
+1. Ordering, speed increases, writeup
--- source/parse.py
+++ source/parse.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
 import pycurl
+import sys
 from StringIO import StringIO
 
 #generate urls
 #TODO: Maybe make this generic?
 pages = 6
+userName = "CommandersBrew"
 decks = []
 for page in range(1, pages+1):
-    pageUri = "http://tappedout.net/users/commandersbrew/mtg-decks/?&p={0}&page={0}".format(page)
+    pageUri = "http://tappedout.net/users/{1}/mtg-decks/?&p={0}&page={0}".format(page, userName)
+    print(pageUri)
     buffer = StringIO()
     c = pycurl.Curl()
     c.setopt(c.URL, pageUri)
@@ -24,28 +27,31 @@ for page in range(1, pages+1):
     print("Downloaded and parsed page {0}".format(page))
 uniqueDecks = list(set(decks))
 uniqueDecks.remove("/mtg-decks/search/")
-uniqueDecks.remove("/accounts/login/?next=/users/commandersbrew/mtg-decks/")
-uniqueDecks.remove("/accounts/register/?next=/users/commandersbrew/mtg-decks/")
+uniqueDecks.remove("/accounts/login/?next=/users/{0}/mtg-decks/".format(userName))
+uniqueDecks.remove("/accounts/register/?next=/users/{0}/mtg-decks/".format(userName))
 
 cards=[]
 for deck in range(0, len(uniqueDecks)):
-    pageUri = "http://tappedout.net{0}".format(uniqueDecks[deck])
-    buffer = StringIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, pageUri)
-    c.setopt(c.WRITEDATA, buffer)
-    c.perform()
-    c.close()
-    body = buffer.getvalue()
-    garbage = body.split("http://store.tcgplayer.com/massentry?partner=TPPDOUT&amp;c=")
-    deckCards = garbage[1].split("\"")[0].split("||")
-    cards= cards + deckCards
-    print("Parsed deck {0}".format(uniqueDecks[deck]))
+    try:
+        pageUri = "http://tappedout.net{0}".format(uniqueDecks[deck])
+        buffer = StringIO()
+        c = pycurl.Curl()
+        c.setopt(c.URL, pageUri)
+        c.setopt(c.WRITEDATA, buffer)
+        c.perform()
+        c.close()
+        body = buffer.getvalue()
+        garbage = body.split("<input type=\"hidden\" name=\"c\" value=\"")
+        deckCards = garbage[1].split("\"")[0].split("||")
+        cards= cards + deckCards
+        print("Parsed deck {0}".format(uniqueDecks[deck]))
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
 print("Parsed {0} decks and found {1} cards".format(len(uniqueDecks), len(cards)))
-totals = {"CardName":0}
+totals = {}
 for card in range(0, len(cards)):
     cardName = cards[card].split("1 ")[1]
-    print(cardName)
+    cardName = cardName.replace("&#39;", "'")
     if cardName in totals:
         totals[cardName] = totals[cardName] + 1
     else:
